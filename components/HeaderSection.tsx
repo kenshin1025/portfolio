f3d272f080a73d132df72e8d977a30b4aa52099b
kenshin1025 <sukoyakamind1025@gmail.com>
2021-12-18 15:34:56 +0900

    最低限自分のページと言い張れる物を作った

---
+++ components/HeaderSection.tsx
@@ -0,0 +1,10 @@
+import styles from "../styles/Home.module.css";
+
+export const HeaderSection = () => {
+  return (
+    <div className="mb-10">
+      <h1 className={styles.title}>けんしんのいろいろ</h1>
+      <p className="text-center">My Portfolio</p>
+    </div>
+  );
+};
